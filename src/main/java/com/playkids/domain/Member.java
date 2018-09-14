81f7c6f1802d1420fe2d235a4eb9f24926017d0c
park <pjhyng0125@naver.com>
2018-09-14 14:31:40 +0900

    update package
    master

---
+++ src/main/java/com/playkids/domain/Member.java
@@ -0,0 +1,5 @@
+package com.playkids.domain;
+
+public class Member {
+
+}
---
+++ src/main/java/com/playkids/persistence/MemberDAO.java
@@ -0,0 +1,8 @@
+package com.playkids.persistence;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+
+}
